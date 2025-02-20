5e81a33c15aa06811bfcac75aa89f04a14011a45
Raoumair5 <230466@students.au.edu.pk>
2025-02-20 19:55:50 +0500

    Addding Cpp file

---
+++ CppFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string name;
+	cout<<"First Code to push in"<<endl;
+	cout<<"enter Name"<<endl;
+	cin>>name;
+	cout<<"Welcome "<<name<<"!"<<endl;
+}
